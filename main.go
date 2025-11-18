d72f14d2fb5d9edb68d5599a28b87371cb7cae5a
ning <zhishidekeqiu@sina.cn>
2025-11-18 21:44:31 +0800

    4

--- main.go
+++ main.go
@@ -1,23 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	str1 := "([[}])"
-	runStr := []rune(str1)
-	runeLen := len(runStr)
-	if runeLen%2 != 0 {
-		fmt.Println("不是有效字符串")
-		return
-	}
-	for i := 0; i < runeLen/2; i++ {
-		newStr := string(runStr[i]) + string(runStr[runeLen-i-1])
-		fmt.Println(newStr)
-		if newStr != "()" && newStr != "[]" && newStr != "{}" {
-			fmt.Println("不是有效字符串")
-			return
-		}
-	}
-	fmt.Println("是有效字符串")
+	str1 := []string{"moon", "monkey", "moonligit"}
 
+	prifix := str1[0]
+	for _, k := range str1 {
+		for strings.Index(k, prifix) != 0 {
+			if len(prifix) == 0 {
+				fmt.Println("没有公共前缀")
+				return
+			}
+			prifix = prifix[:len(prifix)-1]
+		}
+
+	}
+	fmt.Println("公共前缀是", prifix)
 }
