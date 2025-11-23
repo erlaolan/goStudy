f89b56859abc853aeec12a0f7d0adfa0cdd0bc17
ning <zhishidekeqiu@sina.cn>
2025-11-23 10:07:47 +0800

    7

---
+++ task2/7.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func sendOnly(ch chan<- int) {
+	for i := 1; i < 11; i++ {
+		ch <- i
+		fmt.Printf("send---%v\n", i)
+	}
+	close(ch)
+}
+
+func receiveOnly(ch <-chan int) {
+	for val := range ch {
+		fmt.Printf("receive---%v\n", val)
+	}
+}
+
+func main() {
+	var wg sync.WaitGroup
+	ch := make(chan int, 3)
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		sendOnly(ch)
+	}()
+
+	go func() {
+		defer wg.Done()
+		receiveOnly(ch)
+	}()
+
+	wg.Wait()
+
+}
